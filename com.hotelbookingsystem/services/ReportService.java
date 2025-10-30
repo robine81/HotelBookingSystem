7f46bfa5e43b44355a95e4508f47d76e0181df54
Tomas <mac@Macs-MacBook-Air.local>
2025-10-30 15:58:47 +0100

    ReportService Class and to be implemented SQL statement(s)

---
+++ com.hotelbookingsystem/services/ReportService.java
@@ -0,0 +1,33 @@
+package services;
+
+import dao.RoomDAO;
+import dao.RoomDAOImpl;
+
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+public class ReportService {
+    RoomDAO roomDAO = new RoomDAOImpl();
+
+    private void showBookingsPerCustomer () throws SQLException {
+        // SQL Query:
+        // SELECT c.id, c.name, COUNT(DISTINCT b.id) as numberofbookings from customers c INNER join bookings b ON c.id = b.customer_id GROUP BY c.id
+    }
+
+    private void showAveragePriceOnBookings() {
+        // SELECT AVG(r.price) from bookings b INNER JOIN rooms r ON b.room_id = r.id;
+    }
+
+    private void showAvilableRooms() {
+        /*
+        roomDAO.getAvailableRooms(
+                LocalDate.from("2025-11-01"),
+                LocalDate.from("2025-12-01"),
+        ); */
+
+    }
+
+    private void customersWhoNeverBooked () {
+        // SELECT c.id, c.name  from customers c left join bookings b ON c.id = b.customer_id WHERE b.id IS NULL;
+    }
+}
