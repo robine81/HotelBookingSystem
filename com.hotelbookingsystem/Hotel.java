3de7c225bb845bc8dc6e18446d130d4ebf5003f8
Tomas <mac@Macs-MacBook-Air.local>
2025-10-31 14:10:25 +0100

    Make reports printable

--- com.hotelbookingsystem/Hotel.java
+++ com.hotelbookingsystem/Hotel.java
@@ -1,8 +1,6 @@
-import controllers.BookingController;
-import controllers.CustomerController;
-import controllers.InputManagementUtility;
-import controllers.RoomController;
+import controllers.*;
 
+import java.sql.SQLException;
 import java.util.*;
 
 public class Hotel {
@@ -10,12 +8,33 @@ public class Hotel {
     BookingController bookingController = new BookingController();
     RoomController roomController = new RoomController();
     CustomerController customerController = new CustomerController();
+    ReportController reportController = new ReportController();
 
     public void runMenu() {
-        InputManagementUtility.runMenuUntilQuit(new HashMap<>() {{
+        InputManagementUtility.runMenuUntilQuit(new LinkedHashMap<>() {{
             put("Guests", () -> customerController.menu());
             put("Rooms", () -> roomController.menu());
             put("Bookings", () -> bookingController.menu());
+            put("Reports", () -> runReport());
+        }});
+    }
+
+    void runReport() {
+        InputManagementUtility.runMenuUntilQuit(new LinkedHashMap<>() {{
+            put("Number of bookings by customer", () -> {
+                try {
+                    System.out.println(reportController.numberOfBookingsPerCustomerToString());
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            });
+            put ("Average price for bookings", () -> {
+                try {
+                    System.out.printf("Average price for bookings: %.2f%n", reportController.calculateAveragePriceForBookings());
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            });
         }});
     }
 }
